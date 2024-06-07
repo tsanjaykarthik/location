cd7fa9bfd6cf742bec5a77585008a25229f4464f
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-06-07 13:08:58 +0530

    user list and item

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <div>Users broooo!!</div>
-  )
-}
+  const USERS = [
+    {
+      id: "u1",
+      name: "sanjay karthik",
+      image:
+        "https://media.licdn.com/dms/image/C4D03AQHH-mpYIHZ-2Q/profile-displayphoto-shrink_800_800/0/1656848530756?e=2147483647&v=beta&t=xdZZM3YXRcCR0M5JqxwCebWSNlM2KZSQOkClp39Nd6Y",
+      places: 3,
+    }
+  ];
 
-export default Users
+  return <UsersList items={USERS} />;
+};
+
+export default Users;
