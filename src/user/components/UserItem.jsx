d0894904081b05a81ec075d72d28d0f3770440c6
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-06-07 15:32:44 +0530

    css change

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -3,7 +3,7 @@ import Avatar from "../../shared/components/UIElements/Avatar";
 import { Link } from "react-router-dom";
 import Card from "../../shared/components/UIElements/Card";
 
-import "./userItem.css";
+import "./UserItem.css"
 
 const UserItem = (props) => {
   return (
