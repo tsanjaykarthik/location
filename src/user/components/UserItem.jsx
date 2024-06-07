92bff3932fd125ed502224a5213121bc91749274
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-06-07 15:27:55 +0530

    card

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Avatar from "../../shared/components/UIElements/Avatar";
 import { Link } from "react-router-dom";
+import Card from "../../shared/components/UIElements/Card";
+
 import "./userItem.css";
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <Link to= {`/${props.id}/places`}>
-        <div className="user-item__image">
-          <Avatar image={props.image} alt={props.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
-          </h3>
-        </div>
+      <Card className="user-item__content">
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? "place" : "places"}
+            </h3>
+          </div>
         </Link>
-      </div>
+      </Card>
     </li>
   );
 };
