530624368d28da5a44d6e72543f8a1c5a3c6585c
T. sanjay karthik <108615710+tsanjaykarthik@users.noreply.github.com>
2024-08-08 09:56:30 +0530

    check the git connectivity (small text change check)

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -6,7 +6,7 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "sanjay karthik",
+      name: "sanjay karthik hello",
       image:
         "https://media.licdn.com/dms/image/C4D03AQHH-mpYIHZ-2Q/profile-displayphoto-shrink_800_800/0/1656848530756?e=2147483647&v=beta&t=xdZZM3YXRcCR0M5JqxwCebWSNlM2KZSQOkClp39Nd6Y",
       places: 3,
